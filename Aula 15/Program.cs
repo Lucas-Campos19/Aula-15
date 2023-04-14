bdc3bed52f60f5e9187e70317a6a903d7a884d8c
Lucas <lucas.cgoncalves7@gmail.com>
2023-04-14 19:12:52 -0300

    Adicionar arquivos de projeto.

---
+++ Aula 15/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int minutos = 0;
+            Console.WriteLine("Tempo de viagem Belo Horizonte(MG) a Vitoria(ES)");
+            Console.WriteLine("Escolha a forma da viagem escolhida: [a]Avião || [c]Carro || [o]Ônibus");
+            char escolha = char.Parse(Console.ReadLine());
+
+            switch (escolha) 
+            {
+                case 'a':
+                case 'A':
+                    minutos = 50;
+                    break;
+                case 'c':
+                case 'C':
+                    minutos = 800;
+                    break;
+                case 'o':
+                case 'O':
+                    minutos = 600;
+                    break;
+                default:
+                    minutos = 0;
+                    break;
+            }
+            if(minutos == 0)
+            {
+                Console.WriteLine("Tempo de viagem indisponível!!!");
+            }
+            else
+            {
+                Console.WriteLine("O tempo de viagem escolhido é de {0} minutos", minutos);
+            }
+            Console.ReadKey();
+        }
+    }
+}
